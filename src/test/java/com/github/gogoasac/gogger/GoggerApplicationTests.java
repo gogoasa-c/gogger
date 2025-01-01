367ac15414776cb8f4b1fd9b848352064aec8068
Cristian Gogoasa <gogoasac16@gmail.com>
2025-01-01 22:53:43 +0200

    initial commit

---
+++ src/test/java/com/github/gogoasac/gogger/GoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gogoasac.gogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
